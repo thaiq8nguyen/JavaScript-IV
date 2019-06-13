d6212b84ffeaaea006bbf38c321ac29f122704c6
Thai Nguyen <discoverylight@yahoo.com>
2019-06-13 12:23:35 -0700

    added classes to lambda-school.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -26,8 +26,8 @@ class Instructor extends Person {
       return `${student.name} receives a perfect score on ${subject}.`
     }
 
-    static gradeStudent () {
-        return Math.floor(Math.random() * (100 - 1)) + 1  
+    gradeStudent () {
+        return Math.floor(Math.random() * (90 - 50)) + 50
     }
   }
 
@@ -37,7 +37,7 @@ class Student extends Person {
         this.previousBackground = attributes.previousBackground
         this.className = attributes.className
         this.favoriteSubjects = attributes.favoriteSubjects
-        this.grade = 74
+        this.grade = 1
         
     }
 
@@ -54,11 +54,7 @@ class Student extends Person {
     }
 
     graduate () {
-        if (this.grade >= 70) {
-        return true
-        } else {
-        return false
-        }
+        return this.grade >= 70 ? true : false;
     }
 
     
@@ -71,10 +67,86 @@ class ProjectManager extends Instructor {
     }
   
     standUp (slackChannel) {
-      return `${this.name} announces to ${slackChannel}, @channel standy times!`
+      return `${this.name} announces to ${slackChannel}, @channel standup times!`
     }
   
     debugCode (student, subject) {
       return `${student.name}'s code on ${subject}`
     }
   }
+const instructor =  new Instructor(
+    {
+        name: "Dan", 
+        age: 32, 
+        location: "Denver", 
+        specialty: "Redux", 
+        favoriteLanguages: "JS, React", 
+        catchPhrase: "Kittypuff"
+    }
+)
+
+const student = new Student(
+    {
+        name: "Thai", 
+        age: 35, 
+        location: "Los Angeles", 
+        previousBackground: "Developer", 
+        className: "WEB21", 
+        favoriteSubjects: ["Math", "Engineering", "Cooking"]
+    }    
+)
+
+const pm = new ProjectManager(
+    {
+        name: "Mary", 
+        age: 29, 
+        location: "Atlanta", 
+        gradClassName: "CS3", 
+        favoriteInstructor: "Josh",
+        specialty: "Vue", 
+        favoriteLanguages: "Vue, Python", 
+        catchPhrase: "Let's roll"
+    }    
+)
+/* INSTRUCTOR */
+console.log("Instructor", instructor);
+console.log("Instructor speaks ", instructor.speak());
+console.log("Instructor demos ", instructor.demo("React"));
+console.log("Instructor announces a student's grade ", instructor.grade({name: "Thai"}, "JS Fundamental"))
+console.log("*********************************************");
+/*STUDENT*/
+
+console.log("Student ", student);
+console.log("Student speaks ", student.speak());
+console.log("Student's favorite subjects are: ", (student.listsSubjects()).join(", "));
+console.log("PR Assignment announcement: ", student.PRAssignment("CSS"));
+console.log("Sprint Challenge annoucement: ", student.sprintChallenge("Preprocessor"));
+console.log("*********************************************");
+
+/*Project Manager*/
+console.log("PM ", pm);
+console.log("PM StandUp ", pm.standUp("WEB21"));
+console.log("PM Debug Code ", pm.debugCode({name: "John"}, "Javascript"))
+
+console.log("*********************************************");
+console.log("STRETCH")
+let counter = 0;
+while(!student.graduate()) {
+    let studentGrade = instructor.gradeStudent();
+    console.log(`Class grade: ${instructor.name} gives ${student.name} a ${studentGrade} for ${student.className}`);
+    student.grade = student.grade + studentGrade;
+    console.log(`${student.name} receives a ${student.grade} in ${student.className}`)
+    student.graduate() ? console.log(`${student.name} is passing ${student.className}`) : console.log(`${student.name} is not passing ${student.className}`)
+    counter++;
+
+}
+console.log(`Instructor ${instructor.name} graded ${counter} times`);
+// let studentGrade = instructor.gradeStudent();
+// console.log(`Class grade: ${instructor.name} gives ${student.name} a ${studentGrade} for ${student.className}`);
+// student.grade = student.grade + studentGrade;
+// console.log(`${student.name} receives a ${student.grade} in ${student.className}`)
+// student.graduate ? console.log(`${student.name} is passing ${student.className}`) : console.log(`${student.name} is not passing ${student.className}`)
+
+
+
+
